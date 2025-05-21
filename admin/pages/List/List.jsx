2257dbb53c0b55bdb34877dfcc452e03c5e6f17d
Rajesh18 <161973375+Rajesh1828@users.noreply.github.com>
2025-05-21 11:16:57 +0530

    Update List.jsx

--- admin/pages/List/List.jsx
+++ admin/pages/List/List.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 const List = () => {
 
-  const url = "http://localhost:5000"
+  const url = "https://tomato-backend-0h8d.onrender.com"
   const [list, setList]= React.useState([]);
 
   const fetchList = async()=>{
